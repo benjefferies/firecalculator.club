01060f4b4477821ab762e9d9b6414e21d6f4f339
Ben Jefferies <benjefferies@echosoft.uk>
2021-09-19 09:41:58 +0100

    Refactor and theming

---
+++ src/__tests__/service/CurrencyService.test.ts
@@ -0,0 +1,28 @@
+import { expect, use } from 'chai';
+import chaiSubset from 'chai-subset';
+import { formatCurrency, getCurrency } from '../../service/CurrencyService';
+use(chaiSubset);
+
+describe('CurrencyService', () => {
+  const languageGetter = jest.spyOn(window.navigator, 'language', 'get');
+
+  test('format currency GBP', () => {
+    languageGetter.mockReturnValue('en-GB');
+    expect(formatCurrency(10)).to.equal('£10.00');
+  });
+
+  test('format currency USD', () => {
+    languageGetter.mockReturnValue('en-US');
+    expect(formatCurrency(10)).to.equal('$10.00');
+  });
+
+  test('get currency symbol GBP', () => {
+    languageGetter.mockReturnValue('en-GB');
+    expect(getCurrency()).to.be.equal('£');
+  });
+
+  test('get currency symbol USD', () => {
+    languageGetter.mockReturnValue('en-US');
+    expect(getCurrency()).to.be.equal('$');
+  });
+});
